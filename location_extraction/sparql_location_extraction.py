65bb671dc0a2df6b6564044e60d163de50c1500e
mkrnr <info@mkoerner.de>
2016-01-27 11:09:58 +0100

    Reformat SPARQL query strings

--- location_extraction/sparql_location_extraction.py
+++ location_extraction/sparql_location_extraction.py
@@ -17,51 +17,64 @@ match_out = open("/home/martin/rl/data/dbpedia-all.txt", "wb")  # open output
 match_count = 0
 domain_matches = {}
 
+# SPARQL query that 
 location_query = """
-        { SELECT * WHERE {
-      ?match dbo:locationCity ?locationCity .
-      ?locationCity dbp:latitude ?lat .
-      ?locationCity dbp:longitude ?long
-        } }
-      UNION
-        { SELECT * WHERE {
-      ?match dbo:locationCity ?locationCity .
-    ?locationCity geo:lat ?lat .
-    ?locationCity geo:long ?long
-  } }
-
-  UNION
-        { SELECT * WHERE {
-      ?match dbp:parentAgency ?parentAgency .
-    ?parentAgency geo:lat ?lat .
-    ?parentAgency geo:long ?long
-  } }
-  UNION
-        { SELECT * WHERE {
-    ?match geo:lat ?lat .
-    ?match geo:long ?long
-  } }
-  UNION
-        { SELECT * WHERE {
-      ?match dbo:location ?location .
-    ?location geo:lat ?lat .
-    ?location geo:long ?long
-  } }
-  """
+{
+    SELECT * WHERE {
+        ?match dbo:locationCity ?locationCity .
+        ?locationCity dbp:latitude ?lat .
+        ?locationCity dbp:longitude ?long
+    }
+}
+UNION
+{
+    SELECT * WHERE {
+        ?match dbo:locationCity ?locationCity .
+        ?locationCity geo:lat ?lat .
+        ?locationCity geo:long ?long
+    }
+}
+UNION
+{
+    SELECT * WHERE {
+        ?match dbp:parentAgency ?parentAgency .
+        ?parentAgency geo:lat ?lat .
+        ?parentAgency geo:long ?long
+    }
+}
+UNION
+{
+    SELECT * WHERE {
+        ?match geo:lat ?lat .
+        ?match geo:long ?long
+    }
+}
+UNION
+{
+    SELECT * WHERE {
+        ?match dbo:location ?location .
+        ?location geo:lat ?lat .
+        ?location geo:long ?long
+    }
+}
+"""
 query_string = """
-    select * where {
-        { SELECT * WHERE {
-      ?match foaf:homepage  ?url .
-       """ + location_query + """
+SELECT * WHERE {
+    {
+        SELECT * WHERE {
+            ?match foaf:homepage  ?url .
+            """ + location_query + """
         }
-         }
-      UNION
-        { SELECT * WHERE {
-      ?match dbp:url  ?url .
-       """ + location_query + """
-        } }
-   }
-    """
+    }
+    UNION
+    {
+        SELECT * WHERE {
+            ?match dbp:url  ?url .
+            """ + location_query + """
+        }
+    }
+}
+"""
 limit = 5000
 offset = 0
 while True:
@@ -81,7 +94,7 @@ while True:
             match_string = result["url"]["value"] + "\t" + result["match"]["value"] + "\t" + result["lat"]["value"] + "\t" + result["long"]["value"]
             # print match_string
             match_out.write(match_string.encode('utf8') + "\n")
-            print match_string.encode('utf8')
+            # print match_string.encode('utf8')
         offset += limit
     else:
         break;
