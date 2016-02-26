1dd648234ec8f43ded7ae6b61693089993a14e63
mkrnr <info@mkoerner.de>
2016-02-26 09:23:32 +0100

    Add timeout to website_language_extraction

--- feature_extraction/website_language_extraction.py
+++ feature_extraction/website_language_extraction.py
@@ -14,7 +14,7 @@ import argparse
 import langdetect
 import html2text
 from markdown import markdown
-
+import signal
 
 # generate help text for arguments
 parser = argparse.ArgumentParser(description='Extracts languages from the content of URLs given in a JSON file that contains Wikipedia articles and their referenced URLs')
@@ -25,6 +25,18 @@ args = parser.parse_args()
 
 inputfile_path=args.input
 outputfile_path=args.output
+
+
+# timeout handling from: http://stackoverflow.com/a/25027182/2174538
+class TimeoutException(Exception):   # Custom exception class
+    pass
+
+def timeout_handler(signum, frame):   # Custom signal handler
+    raise TimeoutException
+
+# Change the behavior of SIGALRM
+signal.signal(signal.SIGALRM, timeout_handler)
+
 
 url_language_dictionary = {}
 
@@ -38,28 +50,39 @@ url_count = 0
 for article in json_data:
     for url in json_data[article]:
 
+        if url in url_language_dictionary:
+            continue
+
+        # start a timeout counter
+        signal.alarm(10) 
+
         try:
             html = urllib.urlopen(url).read()
-            
             decoded_html = html.decode(errors='ignore')
             markup_text =  html2text.html2text(decoded_html)
             html_from_markup = markdown(markup_text)
             text = ''.join(BeautifulSoup(html_from_markup).findAll(text=True))
 
-            language=detect(text)
+            language = detect(text)
 
             url_language_dictionary[url] = language
 
+            url_count += 1
+            print url_count
         except AttributeError:
             print "language not detected: " + url
+            continue
         except langdetect.lang_detect_exception.LangDetectException:
             print "no features in text: " + url
+            continue
         except IOError:
             print "URL not found: " + url
+            continue
+        except TimeoutException:
+            print "timeout for: " + url
+            continue
 
             
-    if url_count > 1:
-        break
 
 
 # write results to a JSON file
