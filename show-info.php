d2accbb9bb81946e68d46a2422d95f02c0c9f89e
Florian Windhäuser <fwindhaeuser@uni-koblenz.de>
2016-04-21 10:55:44 +0200

    update

--- show-info.php
+++ show-info.php
@@ -9,6 +9,7 @@
         $python="/usr/bin/python2";
       }
     ?>
+	<script type="text/javascript" src="js/d3.min.js"></script>
   </head>
   <body>
 
@@ -18,9 +19,9 @@
 
       if($handle !== false){
          if($new_crawl == true){
-           $article_json = exec($python.' get_article_data.py ' . $article_url . " " . $new_crawl);
+           $article_path = exec($python.' get_article_data.py ' . $article_url . " " . $new_crawl);
          }else{
-           $article_json = exec($python.' get_article_data.py ' . $article_url);
+           $article_path = exec($python.' get_article_data.py ' . $article_url);
          }
       }
       else{
@@ -28,15 +29,56 @@
          return false;
       }
     ?>
+	<div id="datatable"></div>
+	<script>
+	  var article_path = '<?php echo $article_path; ?>';
+	  d3.json("article_path", function (error, data){
+		  function tabulate(data, columns) {
+			var table = d3.select('body').append('table')
+			var thead = table.append('thead')
+			var	tbody = table.append('tbody');
 
+		// append the header row
+		thead.append('tr')
+		  .selectAll('th')
+		  .data(columns).enter()
+		  .append('th')
+		    .text(function (column) { return column; });
+
+		// create a row for each object in the data
+		var rows = tbody.selectAll('tr')
+		  .data(data)
+		  .enter()
+		  .append('tr');
+
+		// create a cell in each row for each column
+		var cells = rows.selectAll('td')
+		  .data(function (row) {
+		    return columns.map(function (column) {
+		      return {column: column, value: row[column]};
+		    });
+		  })
+		  .enter()
+		  .append('td')
+		    .text(function (d) { return d.value; });
+
+	  return table;
+	}
+
+	// render the table(s)
+	tabulate(data, ['classification', 'classification-general']); // 2 column table
+
+		  });
+	</script>
     <script>
-      var article_json_string = '<?php echo $article_json; ?>';
-      document.write("<p>Loaded via Python, displayed via JS: </p>");
-      try {
-        article_json_parsed = JSON.parse(article_json_string);
-      } catch (e) {
-        document.write("JSON is not valid");
-      }
+      var article_path = '<?php echo $article_path; ?>';
+      //document.write("<p>Loaded via Python, displayed via JS: </p>");
+      //try {
+      //  article_json_parsed = JSON.parse(article_json_string);
+      //} catch (e) {
+      //  document.write("JSON is not valid");
+      //}
+      document.write(article_path);
 
       // print out json:
       document.write('<pre id="json"></pre>');
