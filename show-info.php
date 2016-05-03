7373c1227d89e54eb824355a50703ebbe10ff0da
mkrnr <info@mkoerner.de>
2016-05-03 15:50:53 +0000

    Update table display with show-info.php

--- show-info.php
+++ show-info.php
@@ -29,25 +29,25 @@
          return false;
       }
     ?>
-	<div id="datatable"></div>
 	<script>
 	  var article_path = '<?php echo $article_path; ?>';
 	  d3.json('<?php echo $article_path; ?>', function (error, data){
-		  function tabulate(data, columns) {
-			var table = d3.select('body').append('table')
-			var thead = table.append('thead')
-			var	tbody = table.append('tbody');
+
+	function tabulate(data, columns) {
+		var table = d3.select('body').append('table')
+		var thead = table.append('thead')
+		var	tbody = table.append('tbody');
 
 		// append the header row
 		thead.append('tr')
 		  .selectAll('th')
 		  .data(columns).enter()
 		  .append('th')
-		    .text(function (column) { return column.id; });
+		    .text(function (column) { return column; });
 
 		// create a row for each object in the data
 		var rows = tbody.selectAll('tr')
-		  .data(data.objects)
+		  .data(data)
 		  .enter()
 		  .append('tr');
 
@@ -55,7 +55,7 @@
 		var cells = rows.selectAll('td')
 		  .data(function (row) {
 		    return columns.map(function (column) {
-		      return {column: column.id, value: eval('row.'+column.key) };
+		      return {column: column, value: row[column]};
 		    });
 		  })
 		  .enter()
@@ -64,25 +64,23 @@
 
 	  return table;
 	}
-
 	// render the table(s)
-	tabulate(data, ['classification', 'classification-general']); // 2 column table
-
-		  });
+	tabulate(data, ['url', 'classification', 'classification-general', 'ip-location', 'tld-location', 'website-language', 'wikipedia-language']); // 7 column table
+	});
 	</script>
     <script>
-      var article_path = '<?php echo $article_path; ?>';
+      //var article_path = '<?php echo $article_path; ?>';
       //document.write("<p>Loaded via Python, displayed via JS: </p>");
       //try {
       //  article_json_parsed = JSON.parse(article_json_string);
       //} catch (e) {
       //  document.write("JSON is not valid");
       //}
-      document.write(article_path);
+      //document.write(article_path);
 
       // print out json:
-      document.write('<pre id="json"></pre>');
-      document.getElementById("json").innerHTML = JSON.stringify(article_json_parsed, undefined, 2);
+      //document.write('<pre id="json"></pre>');
+      //document.getElementById("json").innerHTML = JSON.stringify(article_json_parsed, undefined, 2);
     </script>
   </body>
 </html>
